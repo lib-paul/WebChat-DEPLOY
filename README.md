3c608150627a05dfe067268f511e2c6f7c1fac62
Paul <libpaul@outlook.com>
2023-10-20 13:07:14 -0300

    Pruebas de deploy 1

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# backend-webchat
- API BACKEND for WEBCHAT APP
+#WEBCHAT FOR FAST USER TO USER COMS
+
+## Tech Stack
+
+**Client:** React JS
+
+**Server:** Node JS, Express JS
+
+**Database:** Mongo DB
+
+## Para correr de forma local
+
+Clonar el proyecto
+
+```bash
+  git clone https://github.com/lib-paul/WebChat-DEPLOY
+```
+
+Ir al directorio del proyecto
+
+```bash
+  cd webchatapp
+```
+
+Instalar dependencias
+
+```bash
+  npm install
+```
+
+```bash
+  cd frontend/
+  npm install
+```
+
+Iniciar el server
+
+```bash
+  npm run start
+```
+
+Iniciar el cliente
+
+```bash
+  //open now terminal
+  cd frontend
+  npm start
+```
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const path = require("path");
 
 const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 
@@ -14,15 +15,27 @@ connectDB();
 
 app.use(express.json()); //Para la info que viene en JSON
 
-//Ruta principal de prueba
-app.get("/", (req, res) => {
-  res.send("API is Running");
-});
-
 //Rutas del usuario
 app.use("/api/user", userRoutes);
 
 app.use("/api/chat", chatRoutes);
+
+// ------------------------------- Deploy -----------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  //Ruta principal de prueba
+  app.get("/", (req, res) => {
+    res.send("API is Running");
+  });
+}
 
 //Errores
 app.use(notFound);
